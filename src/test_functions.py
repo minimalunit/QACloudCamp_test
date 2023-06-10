eeb0dee75966b99cd6f2e4812ffb7b68d65d8505
Mikhail <mikh.lavrukhin@gmail.com>
2023-06-10 17:44:36 +0300

    minor fixes in test names

--- src/test_functions.py
+++ src/test_functions.py
@@ -5,80 +5,80 @@ url = 'https://jsonplaceholder.typicode.com'
 
 
 
-def test_get_posts():
+def test_check_successful_receiving_posts():
     assert get_posts().status_code == 200, 'something went wrong with getting posts'
 
 
-def test_get_posts_id():
-    get_num = 1
-    get_posts_id_responce = get_posts_id(get_num)
-    assert get_posts_id(get_num).status_code == 200, f'something went wrong with getting posts with id {get_num}'
-    get_posts_id_data = get_posts_id_responce.json()
-    assert get_posts_id_data['userId'] == get_num, f'something went wrong with getting posts with id {get_num}'
+def test_check_successful_receiving_id_post():
+    post_id_number = 10
+    get_posts_id_response = get_posts_id(post_id_number)
+    assert get_posts_id(post_id_number).status_code == 200, f'something went wrong with getting posts with id {post_id_number}'
+    get_posts_id_data = get_posts_id_response.json()
+    assert get_posts_id_data['id'] == post_id_number, f'something went wrong with getting posts with id {post_id_number}'
 
 
-def test_get_posts_non_existent_id():
-    get_nenum = 1112392111231
-    assert get_posts_id(get_nenum).status_code == 404, f'post {get_nenum} doens\'t exist'
+def test_check_error_non_existent_id_post():
+    non_existent_post_id_number = 1112392111231
+    assert get_posts_id(non_existent_post_id_number).status_code == 404, f'post {non_existent_post_id_number} doens\'t exist'
 
 
-def test_post_posts():
-    post_posts_responce = post_posts()
-    assert post_posts_responce.status_code == 201, 'something went wrong with posting posts'
+def test_check_successful_post_creation():
+    post_posts_response = post_posts()
+    assert post_posts_response.status_code == 201, 'something went wrong with posting posts'
 
 
-def test_post_posts_empty_id():
-    post_posts_responce = post_posts(jsonId='')
-    assert post_posts_responce.status_code == 400, 'no userId - no post'
+def test_check_error_creation_post_with_empty_id():
+    post_posts_response = post_posts(jsonId='')
+    assert post_posts_response.status_code == 400, 'no userId - no post'
 
 
-def test_post_posts_empty_title():
-    post_posts_responce = post_posts(jsontitle='')
-    assert post_posts_responce.status_code == 400, 'no title - no post'
+def test_check_error_creation_post_with_empty_title():
+    post_posts_response = post_posts(jsontitle='')
+    assert post_posts_response.status_code == 400, 'no title - no post'
 
 
-def test_post_posts_empty_body():
-    post_posts_responce = post_posts(jsonbody='')
-    assert post_posts_responce.status_code == 400, 'no body - no post'
+def test_check_error_creation_post_with_empty_body():
+    post_posts_response = post_posts(jsonbody='')
+    assert post_posts_response.status_code == 400, 'no body - no post'
 
 
-def test_post_posts_space_id():
-    post_posts_responce = post_posts(jsonId=' ')
-    assert post_posts_responce.status_code == 400, 'only space in userId - no post'
+def test_check_error_creation_post_with_space_id():
+    post_posts_response = post_posts(jsonId=' ')
+    assert post_posts_response.status_code == 400, 'only space in userId - no post'
 
 
-def test_post_posts_space_title():
+def test_check_error_creation_post_with_space_title():
     post_posts_responce = post_posts(jsontitle=' ')
     assert post_posts_responce.status_code == 400, 'only space in title - no post'
 
 
-def test_post_posts_space_body():
-    post_posts_responce = post_posts(jsonbody=' ')
-    assert post_posts_responce.status_code == 400, 'only space in body - no post'
+def test_check_error_creation_post_with_space_body():
+    post_posts_response = post_posts(jsonbody=' ')
+    assert post_posts_response.status_code == 400, 'only space in body - no post'
 
 
-def test_post_posts_letter_id():
-    post_posts_responce = post_posts(jsonId='one')
-    assert post_posts_responce.status_code == 400, 'letters in userId - no post'
+def test_check_error_creation_post_with_letter_id():
+    post_posts_response = post_posts(jsonId='one')
+    assert post_posts_response.status_code == 400, 'letters in userId - no post'
 
 
-def test_post_posts_spchar_id():
-    post_posts_responce = post_posts(jsonId='!?')
-    assert post_posts_responce.status_code == 400, 'special characters in userId - no post'
+def test_check_error_creation_post_with_spchar_id():
+    post_posts_response = post_posts(jsonId='!?')
+    assert post_posts_response.status_code == 400, 'special characters in userId - no post'
 
 
-def test_delete_posts():
-    assert delete_posts().status_code == 200, 'something went wrong with deleting posts because there is no such link as /posts for delete request'
+def test_check_delete_posts():
+    assert delete_posts().status_code == 404, 'something went wrong with deleting posts because there is no such link as /posts for delete request'
 
 
-def test_delete_posts_id():
+def test_check_successful_delete_post():
     del_num = 1
     assert delete_posts_id(del_num).status_code == 200, f'something went wrong with deleting post {del_num}'
 
 
-def test_delete_posts_non_existent_id():
-    del_num = 14123465454
-    assert delete_posts_id(del_num).status_code == 400, f'something went wrong with deleting post {del_num}'
+def test_check_error_delete_post_non_existent_id():
+    delete_non_existent_id = 14123465454
+    assert delete_posts_id(delete_non_existent_id).status_code == 400, f'something went wrong with deleting post {delete_non_existent_id}'
 
 
 
